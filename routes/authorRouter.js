bc655b6a303d9ae9f64050a48cf5aae712d012c4
Anoop Wilson <anoopwilson33@yahoo.com>
2024-05-09 15:08:48 +0530

    Update authorRouter.js

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const { getAuthors, getAuthorById, addAuthor, patchAuthor, deleteAuthor } = require('./controllers/authorControllers')
 const router = express.Router()
 
 // Get all authors.
+
+router.get('/', getAuthors)
+
 // Get author by Id.
+
+router.get('/:authorId', getAuthorById)
+
 // Add a new author.
+
+router.post('/:authorId', addAuthor)
+
+
 // Update author details.
+
+router.patch('/:authorId', patchAuthor)
+
 // Delete author.
+
+router.delete('/:authorId', deleteAuthor)
+
 
 
 module.exports = router
