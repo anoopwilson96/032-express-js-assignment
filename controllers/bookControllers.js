7cac6301e7cc4e4597067da2f9b5bcce8f2cbabb
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-26 00:17:03 +0530

    Update bookControllers.js

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -1,21 +1,64 @@
 
-const getBooks = (req, res) => {
-  res.send('Code for: Get all books')
-}
-const getBookById = (req, res) => {
-  res.send('Code for: Get book by ID')
-}
-const addBook = (req, res) => {
-  res.send('Code for: Add a new book')
-}
-const patchBook = (req, res) => {
-  res.send('Code for: Update book details')
-}
-const deleteBook = (req, res) => {
-  res.send('Code for: Delete a book')
-}
+
+const Book = require("../model/bookModel")
 
 
+//to get all books
+
+const getBooks = async (req, res) => {
+  try {
+  const books =  await Book.find({})
+    res.status(200).json(books)
+
+  } catch (error) {
+    res.status(404).send('Error: Not found')
+  }
+}
+
+//get book by ID
+
+const getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params).exec();
+    res.status(200).json(book)
+  } catch (error) {
+    res.status(404).send('Error: Not found')
+  }
+}
+
+// add new book
+
+const addBook = async (req, res) => {
+  try {
+    const bookData = req.body
+    const book = await new Book (bookData);
+                     await book.save();
+    res.status(200).json(book)
+  } catch (error) {
+    res.status(404).send('Error: Failed to add')
+  }
+}
+
+// Edit or patch existing book details
+
+const patchBook = async (req, res) => {
+ try {
+  const updatedBook = await Book.findByIdAndUpdate(req.params.bookId,req.body, {new:true})
+
+   res.status(200).json(updatedBook)
+
+ } catch (error) {
+  res.status(404).send('Error: Failed to update')
+ }
+}
+const deleteBook = async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.bookId)
+    res.status(200).send('Deleted the book')
+  } catch (error) {
+    res.status(404).send('Error: Failed to delete')
+  }
+}
 
 module.exports = {
   getBooks,
