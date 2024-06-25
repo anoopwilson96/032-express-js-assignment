87472e0bd67e81059862e359fe0f88cc8f0be977
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-26 00:17:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const port = 3000
 
 app.use(cors())
 
-
+app.use(express.json())
 app.use('/books', bookRouter)
 app.use('/authors', authorRouter)
 
 
 
-main().then(()=>{console.log("Database connected")}).catch(err => console.log(err));
+main().then(()=>{console.log("Connected")}).catch(err => console.log(err));
 async function main() {
 await mongoose.connect('mongodb+srv://032-express-js-assignment:Te5FNzdonejMdxTg@cluster0.haebtmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 }
