7d125de5ddd7e30f0b190615ce67b1f84f7865b3
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-26 00:17:00 +0530

    Update authorControllers.js

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,21 +1,67 @@
-const getAuthors = (req, res) => {
-  res.send('Code for: Get all authors')
+const Author = require("../model/authorModel");
+
+//code to get all authors
+
+const getAuthors = async (req, res) => {
+  try {
+    const authors =  await Author.find({});
+    res.status(200).json(authors)
+
+  } catch (error) {
+    res.status(404).send('Error in fetching')
+  }
 }
 
-const getAuthorById = (req, res) => {
-  res.send('Code for: Get author by ID')
+// to get author by specific ID
+
+const getAuthorById = async (req, res) => {
+  try {
+  const author =  await Author.findById(req.params.authorId).exec();
+  res.status(200).json(author)
+    
+  } catch (error) {
+    res.status(404).send('Error: Not found')
+  }
 }
 
-const addAuthor = (req, res) => {
-  res.send('Code for: Add a new author')
+// to add new author
+
+const addAuthor = async (req, res) => {
+  try {
+  const authorData = req.body
+  const author = await new Author (authorData);
+                 await author.save()
+  res.status(200).send( "Added" + author)
+  } catch (error) {
+  res.status(200).send('Error: Failed to add')
+  }
 }
 
-const patchAuthor = (req, res) => {
-  res.send('Code for: Update author details')
+// update an author
+
+const patchAuthor = async (req, res) => {
+ try {
+   const updatedAuthor = await Author.findByIdAndUpdate(req.params.authorId,req.body, {new:true})
+
+   res.status(200).json(updatedAuthor)
+
+  } catch (error) {
+  res.status(404).send('Error: Failed to update')
+ }
 }
 
-const deleteAuthor = (req, res) => {
-  res.send('Code for: Delete a author')
+
+
+//delete an author
+
+const deleteAuthor = async (req, res) => {
+  try {
+    await Author.findByIdAndDelete(req.params.authorId)
+    res.status(200).send('Deleted the author')
+  } catch (error) {
+    res.status(404).send('Error: Failed to delete')
+  }
+ 
 }
 
 module.exports = {
