45de1ead2e8de8ee0fb51fcc67b7339ea865a481
Anoop Wilson <anoopwilson33@yahoo.com>
2024-07-05 17:25:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
 const bookRouter = require ('./routes/bookRouter')
 const authorRouter = require ('./routes/authorRouter')
 const userRouter = require('./routes/userRouter')
+const authRouter = require('./routes/authRouter')
 const cors = require('cors')
 const port = 3000
 
 
-app.use(cors())
-
+app.use(cors({
+    credentials: true,
+    origin:true
+  }
+))
 app.use(express.json())
 app.use('/books', bookRouter)
 app.use('/authors', authorRouter)
 app.use('/users',userRouter)
+app.use('/auth',authRouter)
 
 
 
 main().then(()=>{console.log("Connected")}).catch(err => console.log(err));
 async function main() {
-await mongoose.connect('mongodb+srv://032-express-js-assignment:Te5FNzdonejMdxTg@cluster0.haebtmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+await mongoose.connect(process.env.MDB_URL);
 }
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
