c0e43e00487538e1daf4ddbaedffe127bcee17f7
Anoop Wilson <anoopwilson33@yahoo.com>
2024-07-19 21:00:15 +0530

    token error fixing 1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 3000
 //middleware cors() should be on TOP ALWAYS
 app.use(cors({
     credentials: true,  //to exchange cookies
-    origin:'https://032-book-store-assignment.vercel.app/'       //set path or address(www.facebook.com etc.) as security.
+    origin:true        //set path or address(www.facebook.com etc.) as security.
      } ))             //Only from that path is allowed
 
 //middleware to take body from all request 
